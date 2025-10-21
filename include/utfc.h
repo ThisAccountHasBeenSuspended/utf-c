b894e1722b3017c93d50aa0dd0323e7ac079dcdb
ThisAccountHasBeenSuspended <176883156+ThisAccountHasBeenSuspended@users.noreply.github.com>
2025-10-21 08:02:03 +0200

    General improvements

--- include/utfc.h
+++ include/utfc.h
@@ -9,24 +9,26 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include <stdio.h>
-
 typedef enum {
+    /// No error.
     UTFC_ERROR_NONE,
-    UTFC_ERROR_COMPRESS_OOF, // Out-Of-Memory
-    UTFC_ERROR_COMPRESS_LENGTH,
-    UTFC_ERROR_COMPRESS_UNKNOWN,
-    UTFC_ERROR_DECOMPRESS_LENGTH,
-    UTFC_ERROR_DECOMPRESS_UNKNOWN,
+    /// An unknown error occurred.
+    UTFC_ERROR_UNKNOWN,
+    /// A (re)allocation failed.
+    UTFC_ERROR_OUT_OF_MEMORY,
+    /// A character contains an invalid byte.
+    UTFC_ERROR_INVALID_BYTE,
 } UTFC_ERROR;
 
 typedef struct {
     UTFC_ERROR status;
     uint32_t length;
-    char *value;
+    int8_t *value;
 } UTFC_RESULT;
 
 /* ==================== #!PRIVATE!# ==================== */
+
+#define _UTFC_MAX_CHAR_LEN 4
 
 /**
  * Returns the byte length for the next char of a UTF-8 string.
@@ -36,58 +38,39 @@ typedef struct {
  * - `idx` + `return` is the index of the actual char.
  * - The `return` value 0 means that something went wrong.
  */
-static uint8_t _utfc_char_len(const char *value, uint32_t length, uint32_t idx) {
-    uint8_t byte = value[idx];
+static uint32_t _utfc_char_len(const char *value, uint32_t length, uint32_t idx) {
+    const int8_t byte = (int8_t)value[idx];
+    uint8_t char_len = 1;
 
-    // One-byte character (0xxxxxxx)
-    if ((byte & 0x80) == 0x00) {
-        return 1;
+    // Single-byte character
+    if ((byte & 0x80) == 0) return 1;
+    // Two-byte character
+    else if ((byte & 0xE0) == 0xC0) char_len = 2;
+    // Three-byte character
+    else if ((byte & 0xF0) == 0xE0) char_len = 3;
+    // Four-byte character
+    else if ((byte & 0xF8) == 0xF0) char_len = 4;
+    // Invalid start byte
+    else return 0;
+
+    if ((length - idx) < char_len) return 0;
+
+    for (uint32_t i = 1; i < char_len; ++i) {
+        if ((value[idx + i] & 0xC0) != 0x80) {
+            // No valid continuation byte
+            return 0;
+        }
     }
 
-    // Two-byte character (110xxxxx 10xxxxxx)
-    if ((byte & 0xE0) == 0xC0) {
-        // Check the remaining bytes
-        if ((length - idx) < 2) return 0;
-        // Check the second byte
-        byte = value[idx + 1];
-        if ((byte & 0xC0) != 0x80) return 0;
-        return 2;
-    }
-
-    // Three-byte character (1110xxxx 10xxxxxx 10xxxxxx)
-    if ((byte & 0xF0) == 0xE0) {
-        // Check the remaining bytes
-        if ((length - idx) < 3) return 0;
-        // Check the second byte
-        byte = value[idx + 1];
-        if ((byte & 0xC0) != 0x80) return 0;
-        // Check the third byte
-        byte = value[idx + 2];
-        if ((byte & 0xC0) != 0x80) return 0;
-        return 3;
-    }
-
-    // Four-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
-    if ((byte & 0xF8) == 0xF0) {
-        // Check the remaining bytes
-        if ((length - idx) < 4) return 0;
-        // Check the second byte
-        byte = value[idx + 1];
-        if ((byte & 0xC0) != 0x80) return 0;
-        // Check the third byte
-        byte = value[idx + 2];
-        if ((byte & 0xC0) != 0x80) return 0;
-        // Check the third byte
-        byte = value[idx + 3];
-        if ((byte & 0xC0) != 0x80) return 0;
-        return 4;
-    }
-
-    return 0;
+    return char_len;
 }
 
-static inline UTFC_RESULT _utfc_result_init(UTFC_ERROR error) {
-    UTFC_RESULT result = { error, 0, NULL };
+static inline UTFC_RESULT _utfc_result_init() {
+    UTFC_RESULT result = (UTFC_RESULT){
+        .status = UTFC_ERROR_NONE,
+        .length = 0,
+        .value = NULL
+    };
     return result;
 }
 
@@ -101,19 +84,24 @@ void utfc_result_deinit(UTFC_RESULT *result) {
 }
 
 UTFC_RESULT utfc_compress(const char *data, uint32_t length) {
-    UTFC_RESULT result = _utfc_result_init(UTFC_ERROR_NONE);
+    UTFC_RESULT result = _utfc_result_init();
 
     if (length == 0) {
         result.status = UTFC_ERROR_NONE;
+        result.value = (int8_t *)malloc(1);
+        if (result.value != NULL) {
+            result.length = 1;
+            result.value[0] = 0;
+        }
         return result;
     }
 
     const uint32_t header_size = (length / 255) + 1;
-    const uint32_t estimated_size = header_size + length + 1;
+    const uint32_t estimated_size = header_size + length;
     
-    result.value = (char *)malloc(estimated_size);
+    result.value = (int8_t *)malloc(estimated_size);
     if (result.value == NULL) {
-        result.status = UTFC_ERROR_COMPRESS_OOF;
+        result.status = UTFC_ERROR_OUT_OF_MEMORY;
         return result;
     }
 
@@ -122,58 +110,80 @@ UTFC_RESULT utfc_compress(const char *data, uint32_t length) {
     const uint32_t remainder = length % 255;
 
     for (uint32_t i = 0; i < full_chunks; i++) {
-        result.value[write_pos++] = 255;
+        result.value[write_pos++] = (int8_t)255;
     }
     result.value[write_pos++] = remainder;
 
-    uint8_t *cached_prefix = NULL;
+    int8_t *cached_prefix = NULL;
     uint8_t cached_prefix_len = 0;
 
     for (uint32_t read_pos = 0; read_pos < length; read_pos++) {
-        const uint8_t char_len = _utfc_char_len(data, length, read_pos);
+        const int8_t char_len = _utfc_char_len(data, length, read_pos);
         if (char_len == 0) {
-            // TODO: What should happen? 
+            // Something is wrong with the next character, so we return
+            // the maximum of 4 checked bytes as the value to check them.
+            result.status = UTFC_ERROR_INVALID_BYTE;
+
+            // No longer needed.
+            // Instead, we use the pointer starting at read_pos for data.
+            free(result.value);
+
+            uint32_t remaining_bytes = length - read_pos;
+            if (remaining_bytes > _UTFC_MAX_CHAR_LEN) {
+                remaining_bytes = _UTFC_MAX_CHAR_LEN;
+            }
+            result.length = remaining_bytes;
+            result.value = (int8_t *)&data[read_pos];
+
+            return result;
         }
         const uint8_t prefix_len = char_len - 1;
 
         if (prefix_len > 0) {
             bool prefix_changed = (prefix_len != cached_prefix_len);
+
+            // If the length is not different, we check if the bytes are identical.
             if (prefix_changed == false) {
-                for (uint8_t i = 0; i < prefix_len; i++) {
-                    if (cached_prefix[i] != (uint8_t)data[read_pos + i]) {
+                for (int8_t i = 0; i < prefix_len; i++) {
+                    if (cached_prefix[i] != (int8_t)data[read_pos + i]) {
                         prefix_changed = true;
                         break;
                     }
                 }
             }
+
+            // When we have a new prefix, it is cached and written.
             if (prefix_changed == true) {
-                cached_prefix = (uint8_t *)&data[read_pos];
+                cached_prefix = (int8_t *)&data[read_pos];
                 cached_prefix_len = prefix_len;
 
-                for (uint8_t i = 0; i < prefix_len; i++) {
-                    result.value[write_pos++] = data[read_pos + i];
+                for (int8_t i = 0; i < prefix_len; i++) {
+                    result.value[write_pos++] = (int8_t)data[read_pos + i];
                 }
             }
 
             read_pos += prefix_len;
         }
 
-        const uint8_t byte = (uint8_t)data[read_pos];
+        const int8_t byte = (int8_t)data[read_pos];
         result.value[write_pos++] = byte;
         if (byte == '\0') {
-            // End reached
+            // End of string reached
             break;
         }
     }
 
     result.length = write_pos;
-    result.value = (char *)realloc(result.value, write_pos);
+    int8_t *final_value = (int8_t *)realloc(result.value, write_pos);
+    if (final_value != NULL) {
+        result.value = final_value;
+    }
 
     return result;
 }
 
 UTFC_RESULT utfc_decompress(const char *data, uint32_t length) {
-    UTFC_RESULT result = _utfc_result_init(UTFC_ERROR_NONE);
+    UTFC_RESULT result = _utfc_result_init();
 
     // TODO
 
